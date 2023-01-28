8ff3886273092139296ed92d72872d0cf578d499
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-01-28 16:16:26 -0700

    Add daylist to project.

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+
+  let dayListClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0 });
+
+  return (
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text-regular">{props.name}</h2>
+      <h3 className="test--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
+
+function formatSpots(spots) {
+  if(spots === 0) {
+    return `no spots remaining`;
+  } else if (spots === 1) {
+    return `1 spot remaining`
+  } else {
+    return `${spots} spots remaining`
+  }
+}
