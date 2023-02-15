7438687076ef3ceeebf8d3eb1aa8c29f7f4153f5
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-02-14 21:44:26 -0700

    Add count feature. Remove unused imports

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,6 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-
   let dayListClass = classNames("day-list__item", { "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0 });
 
   return (
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day })
+
+  function bookInterview(id, interview) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`api/appointments/${id}`, { interview }).then(() => setState({ ...state, appointments }), updateSpots(state, state.day, appointments))
+  }
+
+  function cancelInterview(id) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`api/appointments/${id}`)
+      .then(() => setState({ ...state, appointments }), updateSpots(state, state.day, appointments))
+
+  }
+
+  function updateSpots(state, day, appointments) {
+    let count = 0;
+    const filteredDay = state.days.filter(days => days.name === day);
+    const filteredAppointments = filteredDay[0].appointments;
+
+    for (const appointmentKey in filteredAppointments) {
+      if (!appointments[filteredAppointments[appointmentKey]].interview) {
+        count += 1;
+      }
+    }
+
+    state.days.map(days => {
+      if (days.name === day) {
+        days.spots = count;
+      }
+      return ('Count changed');
+    });
+
+    return count;
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('api/days'),
+      axios.get('api/appointments'),
+      axios.get('api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+  }, []);
+
+  return { state: state, setDay, bookInterview, cancelInterview }
+};
