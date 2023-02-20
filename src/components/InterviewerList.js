859f5e6724f4f2a0598928cb819b6771efa45d03
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-02-19 19:00:44 -0700

    Add tests for Form component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
 
   const interviewers = props.interviewers.map(interviewer => {
     return (
@@ -26,3 +27,9 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
